c0c32014e3119c6d9d49d4ac7d6e5dc753403a1c
emfls <emfls247@daum.net>
2017-05-16 23:31:48 +0900

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tm7sidwe^+^w0wl8fc2vus6w5bboiuvj-1-vezzoh!eihj&cgh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
